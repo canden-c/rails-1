8a94c0cf0e81a399c2704ea789c3c1eb8f9c0806
caden <nicanshine@gmail.com>
2018-09-14 16:11:43 +0800

    use simple_format helper to decorate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914061722) do
+ActiveRecord::Schema.define(version: 20180914080243) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
